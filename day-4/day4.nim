4e1137834bf6b7cc870d5b7803802acac65f2a62
Thomas Nelson <tanelso2@gmail.com>
2020-12-05 17:03:10 -0800

    Finished day4

--- day-4/day4.nim
+++ day-4/day4.nim
@@ -1,6 +1,8 @@
+import re
 import sets
 import sequtils
 import strutils
+import sugar
 import tables
 
 let entireFile = readFile("input.txt")
@@ -29,11 +31,58 @@ echo "The answer to part 1 is ", part1ValidPassports.len
 
 proc part2Validate(p: passport): bool =
     for (k,v) in p.pairs:
+        # Return false whenever something fails, continue otherwise
         case k:
             of "byr":
-                # do check, return false if fails
-                echo v
-    return false
+                let intVal = parseInt(v)
+                if intVal < 1920 or intVal > 2002:
+                    echo "failing byr ", v
+                    return false
+            of "iyr":
+                let intVal = parseInt(v)
+                if intVal < 2010 or intVal > 2020:
+                    echo "failing iyr ", v
+                    return false
+            of "eyr":
+                let intVal = parseInt(v)
+                if intVal < 2020 or intVal > 2030:
+                    echo "failing eyr ", v
+                    return false
+            of "hgt":
+                if v.endsWith("cm"):
+                    let intVal = parseInt(v[0..^3])
+                    if intVal < 150 or intVal > 193:
+                        echo "failing hgt ", v
+                        return false
+                elif v.endsWith("in"):
+                    let intVal = parseInt(v[0..^3])
+                    if intVal < 59 or intVal > 76:
+                        echo "failing hgt ", v
+                        return false
+                else:
+                    echo "failing hgt ", v
+                    return false
+            of "hcl":
+                if not (v =~ re"#[0..9|a..f]*"):
+                    echo "failing hcl ", v
+                    return false
+            of "ecl":
+                let validOptions = toHashSet(["amb", "blu", "brn", "gry", "grn", "hzl", "oth"])
+                if not validOptions.contains(v):
+                    echo "failing ecl ", v
+                    return false
+            of "pid":
+                if len(v) != 9:
+                    echo "failing pid ", v
+                    return false
+                if not v.allCharsInSet(Digits):
+                    echo "failing pid ", v
+                    return false
+            else:
+                echo "ignoring ", k
+                discard
+    return true
 
 let part2ValidPassports = passports.filter(part1Validate).filter(part2Validate)
 
+echo "The answer to part2 is ", len(part2ValidPassports)
