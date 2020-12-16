dc723c449820d6c70c4b0163c45dbb00f5de515f
Thomas Nelson <tanelso2@gmail.com>
2020-12-15 23:19:13 -0800

    Simplify day6

--- day-6/day6.nim
+++ day-6/day6.nim
@@ -8,19 +8,12 @@ let groups = entireFile.strip().split("\n\n")
 
 proc getQuestionSet(group: string, op: (HashSet[char], HashSet[char]) -> HashSet[char]): HashSet[char] =
     let lines = group.splitLines
-    var ret = lines[0].toHashSet()
-    for line in lines:
-        let s = line.toHashSet()
-        ret = op(ret, s)
-    return ret
+    return lines.map((x) => x.toHashSet).foldl(op(a, b))
 
 let questionSets = groups.map((x) => x.getQuestionSet(union))
 
 proc getSum(qs: seq[HashSet[char]]): int =
-    var sum = 0
-    for s in qs:
-        sum += s.card
-    return sum
+    return qs.foldl(a + b.card, 0)
 
 echo "The answer to part1 is ", getSum(questionSets)
 
