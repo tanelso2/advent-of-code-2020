d4fb134e122d81a000a14df3ca9097919bf5889f
Thomas Nelson <tanelso2@gmail.com>
2021-01-17 21:07:12 -0800

    Argghhh

--- haskell/day4/app/Main.hs
+++ haskell/day4/app/Main.hs
@@ -14,8 +14,11 @@ main = do
     -- TODO: Don't ignore parse errors
     -- ridges <- return $ rights $ map (\x -> parse parseLine "" x) $ lines contents
     -- putStrLn $ (show ridges)
-    putStrLn $ show $ parse parsePassports "" contents
-    putStrLn $ show $ lines contents
+    -- putStrLn $ show $ parse parsePassports "" contents
+    putStrLn $ show $  (\(Right x) -> x) $ parse parsePassportsString "" contents
+    putStrLn $ show $ length $ (\(Right x) -> x) $ parse parsePassportsString "" contents
+    -- putStrLn $ show $ lines contents
+    -- putStrLn $ show $ take 75 (reverse contents)
     -- putStrLn $ "The answer to part one is " ++ (show (part1 ridges))
     -- putStrLn $ "The answer to part two is " ++ (show (part2 ridges))
     hClose handle
@@ -25,7 +28,7 @@ type Passport = [(String, String)]
 
 parsePassports :: Parser [Passport]
 parsePassports = do
-    x <- sepBy1 parsePassport passportSeparator
+    x <- many1 parsePassport
     return x
 
 passportSeparator = emptyLine
@@ -33,7 +36,19 @@ passportSeparator = emptyLine
 parsePassport :: Parser Passport
 parsePassport = do
     x <- sepBy1 parseField fieldSeparator
+    char '\n'
     return $ x
+
+parsePassportsString :: Parser Passport
+parsePassportsString = do
+    sepBy1 parseField ((string "\n\n") <|> string " " <|> string "\n")
+
+-- parsePassportString :: Parser String
+-- parsePassportString = do
+--     y <- sepBy1 (many anyChar) emptyLine
+--     -- x <- many anyChar
+--     -- emptyLine <|> eof
+--     return y
 
 fieldSeparator = newline <|> space
 
